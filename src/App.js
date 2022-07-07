4c145b78eed528abaaf46f477c14546cfa475a26
Edvinas Jusis <jusis.edvinas@gmail.com>
2022-07-07 11:42:37 +0300

    select and compare cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
     { "src": "/img/helmet-1.png" },
@@ -13,6 +14,8 @@ const cardImages = [
 function App() {
     const [cards, setCards] = useState([])
     const [turns, setTurns] = useState(0)
+    const [choiceOne, setChoiceOne] = useState(null)
+    const [choiceTwo, setChoiceTwo] = useState(null)
 
     const shuffleCards = () => {
         const shuffledCards = [...cardImages, ...cardImages]
@@ -23,11 +26,42 @@ function App() {
         setTurns(0)
     }
 
+    // handle a choice
+    const handleChoice = (card) => {
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    }
+
+    // compare 2 selected cards
+    useEffect(() => {
+        if (choiceOne && choiceTwo) {
+
+            if (choiceOne.src === choiceTwo.src) {
+                console.log('those cards match')
+                resetTurn()
+            } else {
+                console.log('those cards do not match')
+                resetTurn()
+            }
+        }
+    }, [choiceOne, choiceTwo])
+
+    const resetTurn = () => {
+        setChoiceOne(null)
+        setChoiceTwo(null)
+        setTurns(prevTurns => prevTurns + 1)
+    }
 
   return (
     <div className='App'>
       <h1>Hello</h1>
         <button onClick={shuffleCards}>New Game</button>
+
+        <div className="card-grid">
+            {cards.map(card => (
+                <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+            ))}
+
+        </div>
     </div>
   );
 }
